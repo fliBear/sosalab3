da5c4fbdd4f50bc93aac30dec59d8cff53b1e8e2
fliBear <75702140+fliBear@users.noreply.github.com>
2023-05-26 15:50:23 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+from Dodatak_A import OperationsManager
+import math
+
+
+class TestDivision(unittest.TestCase):
+    def test_regular(self):
+        om = OperationsManager(10, 5)
+        result = om.perform_division()
+        self.assertEqual(result, 2.0)
+
+    def test_zero(self):
+        om = OperationsManager(10, 0)
+        result = om.perform_division()
+        self.assertTrue(math.isnan(result))
+
+
+if __name__ == "__main__":
+    unittest.main()
