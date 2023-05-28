0b032fd4bc84123f01e6f7bee8b9efaa1008befe
fliBear <75702140+fliBear@users.noreply.github.com>
2023-05-28 11:17:22 +0200

    Add files via upload

--- Dodatak_A.py
+++ Dodatak_A.py
@@ -1,14 +1,18 @@
 import os
 import getpass
+import ast
+import math
 
-class OperationsManager():
 
+class OperationsManager:
     def __init__(self, a: float, b: float) -> None:
         self.a = a
         self.b = b
 
     def perform_division(self) -> float:
         """Divides a with b. If b is zero, returns NaN."""
+        if self.b == 0:
+            return math.nan
         return self.a / self.b
 
 
@@ -17,18 +21,23 @@ def login_success():
     b = float(input("B = "))
     ops_manager = OperationsManager(a, b)
     print(ops_manager.perform_division())
- 
-    expression = input('Enter a mathematical formula to calculate: ')
-    print ("Result: ", eval(expression))
+
+    expression = input("Enter a mathematical formula to calculate: ")
+    print("Result: ", ast.literal_eval(expression))
+
+
+def safeCheckPass(password):
+    f = open("password.txt", "r")
+    saferPassCheck = f.read()
+    return password == saferPassCheck
 
 
 if __name__ == "__main__":
     user = input("Username: ")
     password = getpass.getpass("Password: ")
-    if user != "root" or password != "123":
+    if user != "root" or not safeCheckPass(password):
         print("Wrong username or password!")
         exit(0)
     else:
         print("Login success!")
         login_success()
-
